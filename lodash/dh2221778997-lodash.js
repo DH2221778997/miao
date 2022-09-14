e714f241c2028f2a00ca3fdc3205ce9220a4fb61
daihai <2221778997@qq.com>
2022-09-14 20:22:35 +0800

    修改

--- lodash/dh2221778997-lodash.js
+++ lodash/dh2221778997-lodash.js
@@ -1,14 +1,21 @@
 var dh2221778997 = {
 
-  chunk: function(array, size) {
+  chunk: function(array, size = 1) {
     var newary = []
+    var newsonary =[]
     var t = 0
     for (var i = 0 ; i < array.length; i++) {
-      if ((i + 1) % 3 != 0) {
-        newary[t].push(array[i])
+      if (i == array.length-1) {
+        newary[t] = newsonary
       } else {
-        newary[t].push(array[i])
-        t = t + 1
+        if ((i + 1) % size != 0) {
+          newsonary.push(array[i])
+        } else {
+          newsonary.push(array[i])
+          newary[t] = newsonary
+          t = t + 1
+          newsonary = []
+        }
       }
     }
     return newary
